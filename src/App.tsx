47617cb9dbd34606f30970e3b7fd8b71fa48b0d5
Denis Vasilev <pythocoder@gmail.com>
2024-05-29 19:25:21 +0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+import item from './item'
+import ShopItemFunc from './ShopItemFunc'
+
+function App() {
+  
+  return (
+    <div className="container">
+      <div className="background-element">
+      </div>
+      <div className="highlight-window">
+        <div className='highlight-overlay'></div>
+      </div>
+      <div className="window">
+        <ShopItemFunc item={item} />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/item.ts
@@ -0,0 +1,20 @@
+interface Item {
+    brand: string;
+    title: string;
+    description: string;
+    descriptionFull: string;
+    price: number;
+    currency: string;
+}
+
+
+const item: Item = {
+    brand: 'Tiger of Sweden',
+    title: 'Leonard coat',
+    description: 'Minimalistic coat in cotton-blend',
+    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+    price: 399,
+    currency: '£'
+}
+
+export default item;
